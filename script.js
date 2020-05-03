37f5d7e636167bef0f037760a7f9555c1243ca83
Bingqing Zhao <59756429+BingqingZhao@users.noreply.github.com>
2020-05-03 00:17:05 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    const next = document.getElementById("next");
+
+    // next.addEventListener('click',function(evt){
+    //     evt.preventDefault();
+    //     if()
+    // });
+
+}());
